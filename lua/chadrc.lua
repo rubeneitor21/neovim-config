37b5b1069ae5f0bacd7eeb0fb4625fe3519ebe1a
Rubeneitor21 <rubeneitor2@hotmail.com>
2024-10-04 12:47:41 +0200

    Nueva config!

---
+++ lua/chadrc.lua
@@ -0,0 +1,26 @@
+-- This file needs to have same structure as nvconfig.lua 
+-- https://github.com/NvChad/ui/blob/v2.5/lua/nvconfig.lua
+-- Please read that file to know all available options :( 
+
+---@type ChadrcConfig
+local M = {}
+
+M.base46 = {
+	theme = "chadracula",
+  transparency = true,
+  hl_override = {
+    Visual = {
+      bg = { "purple", -10 }
+    },
+    Comment = {
+      fg = "#990088"
+      -- bg = {"baby_pink"}
+    },
+  },
+	-- hl_override = {
+	-- 	Comment = { italic = true },
+	-- 	["@comment"] = { italic = true },
+	-- },
+}
+
+return M
---
+++ lua/mappings.lua
@@ -0,0 +1,14 @@
+require "nvchad.mappings"
+
+-- add yours here
+
+local map = vim.keymap.set
+
+map("n", ";", ":", { desc = "CMD enter command mode" })
+map("i", "jk", "<ESC>")
+
+map("n", "<C-T>", function ()
+  require("minty.huefy").open()
+end, {})
+
+-- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
