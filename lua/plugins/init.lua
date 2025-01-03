30228c464e6093cd8d8973ce3ace328fe4f6dce0
Ruben Garcalia <rubeneitor2@hotmail.com>
2025-01-03 22:58:37 +0100

    Añadido renderer para markdown

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -26,6 +26,26 @@ return {
     },
   },
 
+  {
+    "nvim-tree/nvim-tree.lua",
+    opts = {
+      git = {
+        enable = true,
+        ignore = false,
+      }
+    }
+  },
+
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+}
+
   -- Color picker
   -- { "nvchad/volt",  lazy = true },
   -- { "nvchad/minty", lazy = true },
