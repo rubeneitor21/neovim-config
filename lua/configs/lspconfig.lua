233992e13e4c05db9627322de80bf75fd5e112a5
Ruben Garcalia <rubeneitor2@hotmail.com>
2025-02-12 00:50:27 +0100

    lsp de c

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "cssls", "astro", "jdtls", "ts_ls", "tailwindcss", "pylsp", "intelephense", "rust_analyzer", "emmet_ls",
+local servers = { "cssls", "astro", "jdtls", "ts_ls", "tailwindcss", "pylsp", "intelephense", "rust_analyzer", "emmet_ls", "clangd",
   "html", "cssls", "gdscript" } -- html
 local nvlsp = require "nvchad.configs.lspconfig"
 
