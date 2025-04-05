1146ccdaf77cd323632f0c514aef2a92e9fb1344
Ruben Garcalia <rubeneitor2@hotmail.com>
2025-04-05 21:54:53 +0200

    Nuevos mappings para git

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -13,5 +13,8 @@ end, {})
 
 map("n", "<leader>lf", ":lua vim.diagnostic.open_float(nil, {focus=true})<CR>", {desc = "LSP Floating diagnostics"})
 
+map("n", "<leader>gtb", ":Gitsigns toggle_current_line_blame", {desc = "Toggle inline git blame"})
+map("n", "<leader>gb", ":Gitsings blame", {desc = "Show aside git blame"})
+
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
